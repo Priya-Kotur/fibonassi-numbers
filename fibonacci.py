5339d6d63ea66c764bd2f4c95128906964d425cf
Priya-Kotur <93483622+Priya-Kotur@users.noreply.github.com>
2022-02-21 19:50:40 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+n=intinputut("how many numbers?")
+n1,n2 = 0,1
+count = 0
+If (nterms<=0):
+    {
+    print("enter a positive number")
+    elif (nterms >= 1):
+        print ("fibonacci sequence upto",nterms, ":")
+        print("n1")
+}        
+        while count < nterms:
+            print(n1)
+            nth = n1+n2
+            n1 = n2
+            n2 = nth
+            count += 1
